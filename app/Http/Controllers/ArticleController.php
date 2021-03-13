0caec9ad780e6d406e22aa3045bb6e0124d057ef
Merryntbh <toovibohlengeorgia@gmail.com>
2021-03-13 17:41:06 +0000

    Project Barata  Laravel  API

---
+++ app/Http/Controllers/ArticleController.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Article;
+
+class ArticleController extends Controller
+{
+    
+    public function index()
+    {
+
+        return Article::all();
+        
+    }
+ 
+    public function show($article)
+    {
+        return Article::find($article);
+    }
+
+    public function store(Request $request)
+    {
+        $article = Article::create($request->all());
+
+        return response()->json($article, 201);
+    }
+
+    public function update(Request $request, Article $article)
+    {
+        $article->update($request->all());
+
+        return response()->json($article, 200);
+    }
+
+    public function delete(Article $article)
+    {
+        $article->delete();
+
+        return response()->json(null, 204);
+    }
+}
---
+++ database/migrations/2021_03_13_131655_create_articles_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateArticlesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('articles', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('title');
+            $table->float('price');
+            $table->text('description');
+            $table->integer('category_id')->unsignedBigInteger();
+            $table->foreign('category_id')->references('id')->on('category');
+            $table->timestamps();
+        });
+
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('articles');
+    }
+}
